4b84f621efc653e7105544309633c33a22f6f1bf
gaobo <304590126@qq.com>
2026-08-25 19:37:40 +0800

    feat: initial import

---
+++ backend/internal/dto/topology_conflict.go
@@ -0,0 +1,22 @@
+package dto
+
+type ConflictQuery struct {
+	ProposalID *uint
+	State      string
+	Type       string
+	Page       int
+	PageSize   int
+}
+
+type DetectConflictRequest struct {
+	ProposalID     uint    `json:"proposal_id" validate:"required,gt=0"`
+	SnapToleranceM float64 `json:"snap_tolerance_m" validate:"omitempty,gt=0,lte=1000"`
+}
+
+type ConflictTransitionRequest struct {
+	To string `json:"to" validate:"required"`
+}
+
+type ApplySuggestionRequest struct {
+	Rationale string `json:"rationale" validate:"max=2000"`
+}
